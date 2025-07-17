1ac466755e3371eeaf19c89dfd65b6cb1a0d739a
piedna <adenipyc@gmail.com>
2025-07-16 21:52:47 -0700

    Create EFPQ_ontbarcoder_convert.sh

---
+++ scripts/EFPQ_ontbarcoder_convert.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##script written to convert the EFPQ bases that were converted by ONTBarcoder
+##NOTE that this is an expected behaviour of ONTBarcoder:
+	#https://github.com/asrivathsan/ONTbarcoder/issues/2
+	
+	#rename the files
+#rename 's/.fa/.fasta/' *.fa
+
+for i in *.fa
+
+	do
+	
+	sed -i '' 's/E/A/g' $i
+	sed -i '' 's/F/G/g' $i
+	sed -i '' 's/Q/C/g' $i
+	sed -i '' 's/P/T/g' $i
+	
+	done
